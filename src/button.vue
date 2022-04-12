436e9382bdf1675463092f32de8d97c1f5fb86d3
liutongyua <liutongyua@foxmail.com>
2022-04-12 17:37:27 +0800

    添加icon并动态展示内容

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,18 @@
 <template>
-  <button class='wb-button'>按钮</button>
+  <button class='wb-button' :class=`icon-${iconPosition}`>
+    <svg class="svg" v-if="icon">
+      <use :xlink:href=`#i-${icon}`></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
 export default {
-  name: "Button"
+  name: "Button",
+  props:['icon','iconPosition']
 }
 </script>
 
@@ -16,6 +24,10 @@ export default {
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
   &:hover {
   border-color: var(--border-color-hover);
 }
@@ -25,6 +37,14 @@ export default {
   &:focus {
   outline: none;
 }
+  .svg{
+    width: 1em;height: 1em;
+  }
+  > .svg{order:1; margin-right: .1em; }
+  > .content{order: 2 }
+  &.icon-right{
+    > .svg{order:2;margin-right: 0; margin-left: .1em;}
+    > .content{order: 1}
+  }
 }
-
 </style>
