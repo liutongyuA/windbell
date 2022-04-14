00550fcfb0d6418a56a907a41730a745f105edf6
liutongyua <liutongyua@foxmail.com>
2022-04-14 11:42:04 +0800

    造第二个轮子-input（封装组件写样式）

---
+++ src/input.vue
@@ -0,0 +1,63 @@
+<template>
+<div class="wrapper">
+  <input type="text" :value="value" :disabled="disabled" :readonly="readonly" :class="{error}">
+  <template v-if="error">
+    <wb-icon  icon="error"></wb-icon>
+    <span class="errorMessage">{{error}}</span>
+  </template>
+</div>
+</template>
+
+<script>
+export default {
+  name: "input",
+  props:{
+    value:{
+      type:String
+    },
+    disabled:{
+      type:Boolean,
+      default:false
+    },
+    readonly:{
+      type:Boolean,
+      default:false
+    },
+    error:{
+      type:String
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+$height: 32px;
+$border-color: #999;
+$border-color-hover: #666;
+$border-radius: 4px;
+$font-size: 12px;
+$box-shadow-color: rgba(0, 0, 0, 0.5);
+$red: #F1453D;
+.wrapper{
+  display: inline-flex;
+  align-items: center;
+  font-size: $font-size;
+  > :not(:last-child) {margin-right: .5em; }
+  >input{
+    height: 32px;
+    border-radius: 4px;
+    border: 1px solid $border-color;
+    padding: 0 8px;
+    font-size: inherit;
+    &:hover { border-color: $border-color-hover; }
+    &:focus { box-shadow: inset 0 1px 3px $box-shadow-color; outline: none; }
+    &[disabled], &[readonly] {border-color: #bbb;color: #bbb;cursor: not-allowed; }
+    .error{
+      border-color: $red;
+    }
+  }
+  .svg { fill: $red; }
+  .errorMessage { color: $red; }
+}
+
+</style>
