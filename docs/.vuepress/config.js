72a97fea762752c082b2903cbc3157dba82da95e
liutongyua <liutongyua@foxmail.com>
2022-04-15 10:11:03 +0800

    使用vuepress搭建官网，完成基本的构建

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    title: '风铃UI',
+    description: '基于Vue的UI框架',
+    themeConfig: {
+        nav: [
+            {text: '主页', link: '/'},
+            {text: '文档', link: '/guide/'},
+            {text: '谷歌', link: 'https://google.com'},
+        ],
+        sidebar: [
+            {
+                title: '入门',
+                children: [
+                    '/install/',
+                    '/start/',
+                ],
+            },
+            {
+                title: '组件',
+                children: [
+                    '/components/button'
+                ],
+            },
+        ]
+    }
+}
