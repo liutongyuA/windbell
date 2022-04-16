c28369a179ad1e42da8a25e79e81167b586e653a
liutongyua <liutongyua@foxmail.com>
2022-04-16 22:06:32 +0800

    栅格组件雏形搭建及重装sass

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import wbButton from "./src/wb-button";
 import ButtonGroup from './src/buttonGroup'
 import wbInput from './src/wb-input'
+import wbRow from './src/wb-row'
+import wbCol from './src/wb-col'
 Vue.component('wb-button',wbButton)
 Vue.component('wb-button-group',ButtonGroup)
 Vue.component('wb-input',wbInput)
+Vue.component('wb-row',wbRow)
+Vue.component('wb-col',wbCol)
 new Vue({
     el:'#app',
     data:{
---
+++ src/wb-col.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="col" :class="[`col-${span}`]">
+  <slot></slot>
+</div>
+</template>
+
+<script>
+export default {
+  name: "wb-col",
+  props:{
+    span:{type:[Number,String]}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.col{
+  height: 100px;
+  width: 50%;
+  background-color: #ddd;
+  border: 1px solid red;
+
+  @for $n from 1 through 24 {
+    &.col-#{$n} {
+      width: ($n / 24) * 100%;
+    }
+    }
+}
+
+</style>
