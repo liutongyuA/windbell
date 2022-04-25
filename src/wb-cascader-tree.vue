78a2941e55853f1889dc770e8640d4f3efdddba0
liutongyua <liutongyua@foxmail.com>
2022-04-25 10:05:15 +0800

    cascader组件样式完成

--- src/wb-cascader-tree.vue
+++ src/wb-cascader-tree.vue
@@ -1,21 +1,66 @@
 <template>
-<div>
-  <div v-for="item in options">
-    {{item.name}}
-    <div v-if="item.children">
-      <wb-cascader-tree :options="item.children"></wb-cascader-tree>
+<div class="cascader-tree">
+  <div class="left">
+    <div  v-for="item in options" @click="seleced = item" class="item">
+      {{item.name}}
+      <wb-icon icon="right" class="icon" v-if="item.children"></wb-icon>
+    </div>
   </div>
+  <div class="right" v-if="x">
+    <wb-cascader-tree :options="x"></wb-cascader-tree>
   </div>
 </div>
 </template>
 
 <script>
+import wbIcon from './wb-icon'
 export default {
   name: "wb-cascader-tree",
+  data(){
+    return{
+      seleced:null
+    }
+  },
+  components:{
+    'wb-icon':wbIcon
+  },
+  computed:{
+    x(){
+      if(this.seleced&&this.seleced.children){
+        return this.seleced.children
+      }else {
+        return false
+      }
+    }
+  },
+
   props:['options']
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.cascader-tree{
+  font-size: 14px;
+  height: 100%;
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  .left{
+    height: 100%;
+    >.item{
+      padding: 0.3em 1em;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      >.icon{
+        margin-left: 1em;
+        transform: scale(0.5);
+      }
+    }
+  }
+  .right{
+    height: 100%;
+    border-left: 1px solid #dfe2e5;
+  }
+}
 </style>
--- src/wb-cascader.vue
+++ src/wb-cascader.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="cascader">
-    <wb-cascader-tree :options="options"></wb-cascader-tree>
-<!--    <slot></slot>-->
+    <div class="trigger" @click="visible =!visible"></div>
+    <div class="popover" v-if="visible">
+      <wb-cascader-tree :options="options"></wb-cascader-tree>
+    </div>
   </div>
 </template>
 
@@ -9,6 +11,11 @@
 import wbCascaderTree from './wb-cascader-tree'
 export default {
   name: "wb-cascader",
+  data(){
+    return{
+      visible:false
+    }
+  },
   props:{
     options:{type:Array,require:true}
   },
@@ -21,6 +28,19 @@ export default {
 
 <style scoped lang="scss">
 .cascader{
-
+  position: relative;
+  .trigger{
+    width: 100px;
+    height: 32px;
+    border:1px solid black;
+  }
+  .popover{
+    height: 200px;
+    white-space: nowrap;
+    position: absolute;
+    top: 100%;
+    background-color: white;
+    box-shadow: 0 0 3px rgba(0,0,0,0.15);
+  }
 }
 </style>
